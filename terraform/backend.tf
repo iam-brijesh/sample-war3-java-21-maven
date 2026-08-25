46a383441a0ac28e37c4255c13e1aa90a0feb5f9
Ubuntu <ubuntu@ip-172-31-1-184.ap-south-1.compute.internal>
2026-08-25 18:56:24 +0000

    Keep microservice application files only

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "hello-world-eks-terraform-state-022267197315"
-    key          = "sample-war/terraform.tfstate"
-    region       = "ap-south-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
